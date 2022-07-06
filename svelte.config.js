3dbe24a7cffbee9a06475452dbcf9d4ea75fa960
plattegruber <gruberplatte@gmail.com>
2022-07-06 16:20:13 -0500

    add netlify support

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,13 @@
-import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			edge: true,
+			split: false
+		})
 	},
 	preprocess: [
 		preprocess({
