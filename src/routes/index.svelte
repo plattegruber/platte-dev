430ae9469db8b669c57d646b7e1419498c6f65e1
plattegruber <gruberplatte@gmail.com>
2022-07-08 18:24:49 -0500

    add a home header

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,19 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<script>
+    import profileImage from '$lib/assets/platte.jpg';
+</script>
+
+<div>
+    <div class="h-32 w-full lg:h-48 bg-slate-800">
+    </div>
+    <div class="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5 bg-emerald-200 rounded">
+            <div class="flex-1">
+                <h1>Hi! I'm Platte.</h1>
+            </div>
+            <div class="flex-1">
+                <img alt="My profile" src={profileImage} />
+            </div>
+        </div>
+    </div>
+</div>
+  
