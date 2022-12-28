f49f7a6e34d6e9cc38caf3fd6ce839fbef937507
plattegruber <gruberplatte@gmail.com>
2022-12-27 21:01:40 -0600

    add jsdoc typing and eager load posts

--- src/routes/blog/+page.js
+++ src/routes/blog/+page.js
@@ -1,13 +1,14 @@
 export async function load() {
-    const allPostFiles = import.meta.glob('./*.{svx,md}');
-    const allPosts = await Promise.all(Object.entries(allPostFiles).map(async ([path, postPromise]) => {
+    /** @type {import('vite/types/importGlob')} */
+    const allPostFiles = import.meta.glob('./*.{svx,md}', { eager: true});
+
+    const allPosts = Object.entries(allPostFiles).map(([path, post]) => {
         const postPath = path.slice(2, -4);
-        var post = await postPromise()
         return { ...post.metadata, path: postPath, published: post.metadata.date };
-    }));
+    });
     const posts = allPosts.sort((a, b) => b.published - a.published);
     if (!posts || !posts.length) {
-        return { status: 404 };
+        return { posts: [] };
     }
     return { posts };
 }
